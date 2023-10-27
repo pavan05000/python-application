9c6947361441512ed00c331407473274e34fba24
root <root@ip-172-31-32-52.ap-south-1.compute.internal>
2023-10-27 06:45:29 +0000

    text

---
+++ text.py
@@ -0,0 +1,13 @@
+#Import Flask, if not then install and import.
+import os
+try:
+  from flask import *
+except:
+  os.system("pip3 install flask")
+  from flask import *
+app = Flask(__name__)
+@app.route("/")
+def index():
+  return "<h1>Hello World</h1>"
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000, debug=False)
