74096dc7c785381a6b54bddd651ab56a1ddb406d
pavan05000 <122016458+pavan05000@users.noreply.github.com>
2023-10-28 10:19:01 +0530

    Update text.py

--- text.py
+++ text.py
@@ -8,6 +8,6 @@ except:
 app = Flask(__name__)
 @app.route("/")
 def index():
-  return "<h1>checking1</h1>"
+  return "<h1>changes using jenkins</h1>"
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=False)
