e86f4f55e4f1e626dc97ba0a2e22e05c5240d836
pavan05000 <122016458+pavan05000@users.noreply.github.com>
2023-10-30 13:06:10 +0530

    Update text.py

--- text.py
+++ text.py
@@ -8,6 +8,6 @@ except:
 app = Flask(__name__)
 @app.route("/")
 def index():
-  return "<h1>changes making from github</h1>"
+  return "<h1>hello world</h1>"
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=False)
